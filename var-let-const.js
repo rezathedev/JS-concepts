5e150d9d94d7489d588aee179b2e613191b2fee9
Md Reza <i.mdreza1992@gmail.com>
2023-01-31 20:23:26 +0100

    function

--- var-let-const.js
+++ var-let-const.js
@@ -20,6 +20,52 @@ function inchToFeet(inches){
 const dadaInch = 144;
 const dadaFeet = inchToFeet(dadaInch);
 
-console.log(dadaFeet);
+// console.log(dadaFeet);
+
+function mileToKm (miles){
+    const km = miles * 1.6;
+    return km;
+}
+
+let mileNumber = 188
+const calculator = mileToKm(mileNumber)
+
+// console.log (calculator)
+
+// function oddEven (number){
+//     if number % 2 == 0;
+//     return true;
+// }
+
+// let myNumber = 12
+// let isEven = oddEven(myNumber);
+
+// console.log(isEven)
+
+// function isEven (number){
+//     const remainder = number % 2;
+//     // console.log(remainder);
+//     if (remainder === 0){
+//         console.log ("I am Even");
+//     }
+//     else {
+//         console.log ("I am odd");
+//     }
+// }
+ 
+// isEven(1001)
+
+function oddEven (number){
+    const remainder = number % 2;
+    if (remainder === 0){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+let inputNumber = 144;
+let myNumber = oddEven(inputNumber);
 
 
+console.log(myNumber)
