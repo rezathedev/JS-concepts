a5452b705c8622b56887afea8b65d975ae5ea9ab
Md Reza <i.mdreza1992@gmail.com>
2023-01-31 19:08:09 +0100

    first commit

---
+++ recap.js
@@ -0,0 +1,39 @@
+//variables
+var age = 30;
+var myName = "Reza";
+
+//math operations + - * /
+
+//shorthand += -= *= /=
+// ++ --
+
+//Aarry 
+
+var friends = ["Reza", "Shimu", "Saddam", "Islam"]
+
+var myThirdFriend = friends[2];
+
+friends[2] = "Hello"
+
+//Conditionals > < <= >= == !=
+
+if(friends.length < 2) {
+    // console.log ("I dont have friends")
+}
+else {
+    // console.log ("I have many friends")
+}
+
+//4. loop
+
+var roll = 0;
+
+while (roll <= 5){
+    // console.log ("I am the king");
+    roll++;
+}
+
+for (var i = 0; i < 5; i++){
+    var friend = friends[i]
+    // console.log(friend);
+}
